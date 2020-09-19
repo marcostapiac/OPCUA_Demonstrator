2fafb8ac917c75a0e20c9f915576db469e75621f
marcostapiac <52489697+marcostapiac@users.noreply.github.com>
2020-09-19 14:13:52 +0100

    Updated myServer2

--- Phase2/myServer2.py
+++ Phase2/myServer2.py
@@ -1,8 +1,9 @@
-import socket, time
+import socket
+import time
 from opcua import Server, ua, Client, uamethod
 from opcua.ua import BrowseDirection
 from opcua.ua.attribute_ids import AttributeIds
-from opcua.ua.uatypes import ValueRank # Library where we can find different ua types (EventNotifiers, etc)
+from opcua.ua.uatypes import ValueRank  # Library where we can find different ua types (EventNotifiers, etc)
 from opcua.common.ua_utils import value_to_datavalue
 
 
@@ -12,7 +13,7 @@ class ExtendedServer(Server):
 
     def __init__(self, port):
         super().__init__()
-        ip = socket.gethostbyname(socket.gethostname())
+        ip = "192.168.41.126"
         self.url = "opc.tcp://" + str(ip) + ":" + str(port) + "/freeopcua/server/"
         self.set_endpoint(self.url)
 
@@ -45,7 +46,6 @@ class ExtendedServer(Server):
                     val = parentNode.call_method(methodNode, val.Value.Value)
                     TempHistory.append(val)  # Latest value is at 0th index
 
-        print("Temperature History (°C): {}".format(TempHistory))  # Print calibrated data
         return TempHistory
 
     def connectToClient(self, port, ip=""):
@@ -68,6 +68,17 @@ class ExtendedServer(Server):
         variable.set_attribute(AttributeIds.Historizing, value_to_datavalue(Historizing))
         return variable
 
+    def addProperty(self, parentNode, browseName, value, Access, UserAccess, ValueRankValue, Historizing):
+        """ Function to add Data Variables, which represent the content of an Object """
+        # 10 Float datatype, 12 String datatype
+        variable = parentNode.add_property(self.idx, browseName, value)
+        # 1 Current R, 3 Current RW, 15 Current RW & History RW
+        variable.set_attribute(AttributeIds.AccessLevel, value_to_datavalue(Access))
+        variable.set_attribute(AttributeIds.UserAccessLevel, value_to_datavalue(UserAccess))
+        variable.set_attribute(AttributeIds.ValueRank, value_to_datavalue(ValueRankValue))
+        variable.set_attribute(AttributeIds.Historizing, value_to_datavalue(Historizing))
+        return variable
+
 
 if __name__ == "__main__":
 
@@ -75,7 +86,8 @@ if __name__ == "__main__":
     Root = server.get_root_node()
     ObjectsRoot = Root.get_children()[0]
     MovingAverageNode = ObjectsRoot.add_object(server.idx, "MovingAverage", objecttype=ua.ObjectIds.BaseObjectType)
-    MovingAverageNode.set_attribute(AttributeIds.EventNotifier, value_to_datavalue(1)) # Subscribable node
+    MovingAverageNode.set_attribute(AttributeIds.EventNotifier, value_to_datavalue(1))  # Subscribable node
+    EngineeringUnit = server.addProperty(MovingAverageNode, "EngineeringUnit", 0, 1, 1, ValueRank.Scalar, False)
     Value = server.addVariable(MovingAverageNode, "ValueNode", 0.0, 10, 15, 15, ValueRank.Scalar, True)
 
     try:
@@ -86,18 +98,20 @@ if __name__ == "__main__":
         server.historize_node_data_change(Value, count=1)  # Data change only true for Variables
 
         methodNode = server.createMovingAverageTemperatureMethod(ObjectsRoot)
-        client = server.connectToClient(4840)  # Connect to Server1
+        client = server.connectToClient(4840, "192.168.41.126")  # Connect to Server1
 
         while True:
             ObjectsRoot1 = client.get_root_node().get_children()[0]
             TemperatureSensor = ObjectsRoot1.get_child(["2:TemperatureSensor"])  # Return Temperature Sensor in other
             # Server via Browse Name
+            unit = TemperatureSensor.get_child(["2:EngineeringUnit"]).get_value()  # Get Sensor Unit
+            EngineeringUnit.set_attribute(AttributeIds.Value, value_to_datavalue(unit))  # Write to property
             calibrationMethod = ObjectsRoot1.get_methods()[0]  # Access Methods in Server 1
             TemperatureHistory = server.obtainHistoricalValues(TemperatureSensor, ObjectsRoot1, calibrationMethod)
             movingAverage = ObjectsRoot.call_method(methodNode, TemperatureHistory)
             Value.set_attribute(AttributeIds.Value,
                                 value_to_datavalue(movingAverage))  # Update MovingAverage Node Value
-            print("{}\n".format(Value.get_value()))
+            print("{}{}\n".format(Value.get_value(), unit))
             time.sleep(0.5)  # Same time delay as Temperature Sensor
 
     except Exception as e:
